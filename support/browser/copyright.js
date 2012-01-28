13d98dd92d62406e83343bcf503d3b61af19aaa2
Jake Luer <jake.luer@gmail.com>
2012-01-27 22:17:42 -0500

    copyright update

--- support/browser/copyright.js
+++ support/browser/copyright.js
@@ -1,6 +1,6 @@
-/*!
+/**
  * filtr - MongoDB inspired array filtering
  * Copyright(c) 2012 Jake Luer <jake@alogicalparadox.com>
+ * @see https://github.com/logicalparadox/filtr
  * MIT Licensed
  */
-
